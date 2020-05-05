717d3759ac32e543c82ff2dda5db0462af6de7bc
anshimko <54039086+anshimko@users.noreply.github.com>
2020-05-05 19:18:33 +0300

    Update Solution.java

--- src/com/senla/task05/Solution.java
+++ src/com/senla/task05/Solution.java
@@ -3,12 +3,6 @@ package com.senla.task05;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-/*
-5. Создать программу, которая в последовательности от 0 до N, находит все числа-палиндромы 
-(зеркальное значение равно оригинальному). 
-Длина последовательности N вводится вручную и не должна превышать 100. 
-*/
-
 public class Solution {
 
 	public static void main(String[] args) {
